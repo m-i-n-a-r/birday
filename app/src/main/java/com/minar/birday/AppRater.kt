705ee5411c2f9ab32c456b6c1b66ede421ebc996
m-i-n-a-r <glucaconti.952@gmail.com>
2020-04-06 19:42:59 +0200

    Initial commit

---
+++ app/src/main/java/com/minar/birday/AppRater.kt
@@ -0,0 +1,82 @@
+package com.minar.birday
+
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.net.Uri
+import com.afollestad.materialdialogs.LayoutMode
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.bottomsheets.BottomSheet
+import com.minar.birday.R
+
+object AppRater {
+
+    private const val DO_NOT_SHOW_AGAIN = "do_not_show_again" // package name
+    private const val APP_RATING = "app_rating" // package name
+    private const val LAUNCH_COUNT = "launch_count" // package name
+    private const val DATE_FIRST_LAUNCH = "date_first_launch" // package name
+
+    private const val DAYS_UNTIL_PROMPT = 2 // min number of days
+    private const val LAUNCHES_UNTIL_PROMPT = 4 // min number of launches
+
+    private const val DIALOG_CORNER = 16.toFloat()
+
+    @JvmStatic
+    fun appLaunched(context: Context) {
+        val prefs = context.getSharedPreferences(APP_RATING, 0)
+        if (prefs.getBoolean(DO_NOT_SHOW_AGAIN, false)) {
+            return
+        }
+
+        val editor = prefs.edit()
+
+        // increment launch counter
+        val launchCount = prefs.getLong(LAUNCH_COUNT, 0) + 1
+        editor.putLong(LAUNCH_COUNT, launchCount)
+
+        // get date of first launch
+        var dateFirstLaunch = prefs.getLong(DATE_FIRST_LAUNCH, 0)
+        if (dateFirstLaunch == 0L) {
+            dateFirstLaunch = System.currentTimeMillis()
+            editor.putLong(DATE_FIRST_LAUNCH, dateFirstLaunch)
+        }
+
+        // Wait at least n days before opening
+        if (launchCount >= LAUNCHES_UNTIL_PROMPT) {
+            if (System.currentTimeMillis() >= dateFirstLaunch + DAYS_UNTIL_PROMPT * 24 * 60 * 60 * 1000) {
+                showRateDialog(context, editor)
+            }
+        }
+
+        editor.apply()
+    }
+
+    private fun showRateDialog(context: Context, editor: SharedPreferences.Editor) {
+        MaterialDialog(context, BottomSheet(LayoutMode.WRAP_CONTENT))
+                .show {
+
+                    cornerRadius(DIALOG_CORNER)
+                    title(R.string.leave_review_title)
+                    message(R.string.leave_review_subtitle)
+                    positiveButton(R.string.yes_review) {
+                        context.startActivity(
+                                Intent(
+                                        Intent.ACTION_VIEW,
+                                        Uri.parse("market://details?id=${context.packageName}")
+                                )
+                        )
+                        editor.putBoolean(DO_NOT_SHOW_AGAIN, true)
+                        editor.commit()
+                        dismiss()
+                    }
+                    negativeButton(R.string.later) {
+                        dismiss()
+                    }
+                    neutralButton(R.string.no_thanks) {
+                        editor.putBoolean(DO_NOT_SHOW_AGAIN, true)
+                        editor.commit()
+                        dismiss()
+                    }
+                }
+    }
+}
---
+++ app/src/main/java/com/minar/birday/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.minar.birday
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
