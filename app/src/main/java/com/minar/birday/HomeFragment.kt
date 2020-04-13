94cd0b913f0df5d8660291659a4f8f63a9988b93
m-i-n-a-r <glucaconti.952@gmail.com>
2020-04-13 14:32:57 +0200

    Fragments are properly displayed, new icon

--- app/src/main/java/com/minar/birday/HomeFragment.kt
+++ app/src/main/java/com/minar/birday/HomeFragment.kt
@@ -7,19 +7,10 @@ import android.view.View
 import android.view.ViewGroup
 
 class HomeFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+        inflater.inflate(R.layout.fragment_home, container, false)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
     }
-
-    companion object {
-        fun newInstance(): HomeFragment {
-            return HomeFragment()
-        }
-    }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
-
 }
