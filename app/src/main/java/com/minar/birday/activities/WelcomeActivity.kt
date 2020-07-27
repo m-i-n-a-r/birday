94f18d81584e22dfb3ab99e0315070e459bf0683
m-i-n-a-r <glucaconti.952@gmail.com>
2020-07-27 17:05:07 +0200

    Spare fixes

--- app/src/main/java/com/minar/birday/activities/WelcomeActivity.kt
+++ app/src/main/java/com/minar/birday/activities/WelcomeActivity.kt
@@ -13,6 +13,7 @@ import com.minar.birday.R
 class WelcomeActivity : AppIntro() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // TODO wait for a non-deprecated solution which works on every Android version
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
         showIntroSlides()
     }
