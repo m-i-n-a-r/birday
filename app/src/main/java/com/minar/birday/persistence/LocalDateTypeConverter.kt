6ece7a6a1f120aee9d9c977baa4b9100c662fad8
m-i-n-a-r <glucaconti.952@gmail.com>
2021-01-01 17:57:40 +0100

    Leap year crash fixed

--- app/src/main/java/com/minar/birday/persistence/LocalDateTypeConverter.kt
+++ app/src/main/java/com/minar/birday/persistence/LocalDateTypeConverter.kt
@@ -1,12 +1,21 @@
 package com.minar.birday.persistence
 
 import androidx.room.TypeConverter
+import java.lang.Exception
 import java.time.LocalDate
 
 class LocalDateTypeConverter {
     @TypeConverter
-    fun stringToLocalDate(value: String?): LocalDate? {
-        return if (value == null) null else LocalDate.parse(value)
+    fun stringToLocalDate(value: String?): LocalDate {
+        // Extra steps to avoid crashes for leap years
+        return try {
+            if (value == null) LocalDate.now()
+            else LocalDate.parse(value)
+        } catch (e: Exception) {
+            if (value!!.substring(5) == "02-29")
+                LocalDate.parse("${value.substring(0,5)}03-01")
+            else LocalDate.now()
+        }
     }
 
     @TypeConverter
