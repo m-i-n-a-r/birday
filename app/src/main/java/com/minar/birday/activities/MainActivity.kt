2e14b183f0df44ce1946aa7997dd78f852194298
m-i-n-a-r <glucaconti.952@gmail.com>
2021-04-25 01:16:59 +0200

    Go back on navigation item reselected

--- app/src/main/java/com/minar/birday/activities/MainActivity.kt
+++ app/src/main/java/com/minar/birday/activities/MainActivity.kt
@@ -158,7 +158,8 @@ class MainActivity : AppCompatActivity() {
             true
         }
         navigation.setOnNavigationItemReselectedListener {
-            // Just ignore the reselection of the same item
+            // Only do something if there's something in the back stack (only in event details)
+            if (navController.currentBackStackEntry != null) navController.popBackStack()
         }
 
         // Rating stuff
