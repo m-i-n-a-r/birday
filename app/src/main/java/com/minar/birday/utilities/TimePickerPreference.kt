a24d4fd2793d59e3b2e021c548fc0838493da50d
m-i-n-a-r <glucaconti.952@gmail.com>
2021-05-09 02:15:25 +0200

    Timepicker themed and improved

--- app/src/main/java/com/minar/birday/utilities/TimePickerPreference.kt
+++ app/src/main/java/com/minar/birday/utilities/TimePickerPreference.kt
@@ -13,44 +13,67 @@ import com.google.android.material.timepicker.TimeFormat
 import com.minar.birday.R
 import com.minar.birday.activities.MainActivity
 import com.minar.birday.databinding.TimePickerRowBinding
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
 
 // A custom preference to show a time picker
 class TimePickerPreference(context: Context?, attrs: AttributeSet?) : Preference(context, attrs),
     View.OnClickListener {
     private lateinit var sharedPrefs: SharedPreferences
-    private lateinit var currentTime: String
+    private lateinit var currentHour: String
+    private lateinit var currentMinute: String
     private lateinit var binding: TimePickerRowBinding
+    val formatter: DateTimeFormatter = DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT)
 
     override fun onBindViewHolder(holder: PreferenceViewHolder) {
         sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context)
-        currentTime = sharedPrefs.getString("notification_hour", "8").toString()
+        currentHour = sharedPrefs.getString("notification_hour", "8").toString()
+        currentMinute = sharedPrefs.getString("notification_minute", "0").toString()
         super.onBindViewHolder(holder)
         binding = TimePickerRowBinding.bind(holder.itemView)
 
+        // Format the time correctly
+        val currentTime = LocalTime.of(currentHour.toInt(), currentMinute.toInt())
+
         binding.timePickerDescription.text =
-            String.format(context.getString(R.string.notification_hour_description), currentTime)
+            String.format(
+                context.getString(R.string.notification_hour_description),
+                "~${formatter.format(currentTime)}"
+            )
+
         binding.root.setOnClickListener(this)
     }
 
     override fun onClick(v: View) {
         val act = context as MainActivity
-        currentTime = sharedPrefs.getString("notification_hour", "8").toString()
+        currentHour = sharedPrefs.getString("notification_hour", "8").toString()
+        currentMinute = sharedPrefs.getString("notification_minute", "0").toString()
+
+        // Show the time picker
         val isSystem24Hour = is24HourFormat(context)
         val clockFormat = if (isSystem24Hour) TimeFormat.CLOCK_24H else TimeFormat.CLOCK_12H
         val picker =
             MaterialTimePicker.Builder()
                 .setTimeFormat(clockFormat)
-                .setHour(currentTime.toInt())
-                .setMinute(0)
+                .setHour(currentHour.toInt())
+                .setMinute(currentMinute.toInt())
                 .setTitleText(context.getString(R.string.notification_hour_name))
                 .build()
 
         picker.addOnPositiveButtonClickListener {
-            sharedPrefs.edit().putString("notification_hour", picker.hour.toString()).apply()
+            val editor = sharedPrefs.edit()
+            editor.putString("notification_hour", "${picker.hour}")
+            editor.putString("notification_minute", "${picker.minute}")
+            editor.apply()
+
+            // Format the selected hour and update the text
+            val currentTime = LocalTime.of(picker.hour, picker.minute)
+
             binding.timePickerDescription.text =
                 String.format(
                     context.getString(R.string.notification_hour_description),
-                    picker.hour.toString()
+                    "~${formatter.format(currentTime)}"
                 )
         }
 
