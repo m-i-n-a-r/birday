1550534ea2852983270bf351cb35c9977b734667
minar <minar.tastic@gmail.com>
2022-11-29 13:30:50 +0100

    Always expand the insert bottom sheet

--- app/src/main/java/com/minar/birday/fragments/dialogs/InsertEventBottomSheet.kt
+++ app/src/main/java/com/minar/birday/fragments/dialogs/InsertEventBottomSheet.kt
@@ -21,6 +21,8 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.toBitmap
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.android.material.datepicker.CalendarConstraints
 import com.google.android.material.datepicker.MaterialDatePicker
@@ -70,6 +72,9 @@ class InsertEventBottomSheet(
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        // Fully expand the dialog
+        (dialog as BottomSheetDialog).behavior.state = BottomSheetBehavior.STATE_EXPANDED
+
         // Animate the drawable in loop
         val titleIcon = binding.insertEventImage
         val title = binding.insertEventTitle
