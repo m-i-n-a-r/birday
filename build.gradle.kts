9a56fff5befba4c68cd28f0aeadf0cc6623a250c
m-i-n-a-r <glucaconti.952@gmail.com>
2024-07-09 00:41:46 +0200

    We have a shit ton of fixes to do

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,5 @@ plugins {
     id("com.android.application") version "8.5.0" apply false
     id("org.jetbrains.kotlin.android") version "2.0.0" apply false
     id("com.google.devtools.ksp") version "2.0.0-1.0.22" apply false
-    id("androidx.navigation.safeargs.kotlin") version "2.8.0-beta03" apply false
+    id("androidx.navigation.safeargs.kotlin") version "2.8.0-beta04" apply false
 }
