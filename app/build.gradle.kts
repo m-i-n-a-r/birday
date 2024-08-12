7331426adb0f3b1e46ad26bf5e464ac46b7af043
minar <minar.tastic@gmail.com>
2024-08-12 16:54:27 +0200

    Libraries updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -98,14 +98,14 @@ dependencies {
     implementation("androidx.core:core-splashscreen:1.0.1")
 
     // Material Components
-    implementation("com.google.android.material:material:1.13.0-alpha04")
+    implementation("com.google.android.material:material:1.13.0-alpha05")
 
     // WorkManager
-    implementation("androidx.work:work-runtime-ktx:2.9.0")
+    implementation("androidx.work:work-runtime-ktx:2.9.1")
 
     // Navigation component
-    implementation("androidx.navigation:navigation-fragment-ktx:2.8.0-beta06")
-    implementation("androidx.navigation:navigation-ui-ktx:2.8.0-beta06")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.0-beta07")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.0-beta07")
 
     // Lifecycle and ViewModel
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
