c5a30819a91361069c3c8cb0745ab9143c77d64d
minar <minar.tastic@gmail.com>
2024-09-24 12:44:45 +0200

    Libraries and Gradle updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -85,8 +85,8 @@ dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.preference:preference-ktx:1.2.1")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("androidx.activity:activity-ktx:1.9.1")
-    implementation("androidx.fragment:fragment-ktx:1.8.2")
+    implementation("androidx.activity:activity-ktx:1.9.2")
+    implementation("androidx.fragment:fragment-ktx:1.8.3")
 
     // Transition
     implementation("androidx.transition:transition-ktx:1.5.1")
@@ -98,18 +98,18 @@ dependencies {
     implementation("androidx.core:core-splashscreen:1.0.1")
 
     // Material Components
-    implementation("com.google.android.material:material:1.13.0-alpha05")
+    implementation("com.google.android.material:material:1.13.0-alpha06")
 
     // WorkManager
     implementation("androidx.work:work-runtime-ktx:2.9.1")
 
     // Navigation component
-    implementation("androidx.navigation:navigation-fragment-ktx:2.8.0-rc01")
-    implementation("androidx.navigation:navigation-ui-ktx:2.8.0-rc01")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.1")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.1")
 
     // Lifecycle and ViewModel
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.4")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.6")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
 
     // Room
