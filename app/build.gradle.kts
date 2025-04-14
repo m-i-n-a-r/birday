08dc1e4e7f0be5ff6259577474f471834403d040
minar <minar.tastic@gmail.com>
2025-04-14 11:00:18 +0200

    Libraries and Gradle updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,38 +82,38 @@ dependencies {
 
     // Default dependencies
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.preference:preference-ktx:1.2.1")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("androidx.activity:activity-ktx:1.9.3")
-    implementation("androidx.fragment:fragment-ktx:1.8.5")
+    implementation("androidx.activity:activity-ktx:1.10.1")
+    implementation("androidx.fragment:fragment-ktx:1.8.6")
 
     // Transition
     implementation("androidx.transition:transition-ktx:1.5.1")
 
     // Constraint / motion layout
-    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
 
     // Splashscreen
     implementation("androidx.core:core-splashscreen:1.0.1")
 
     // Material Components
-    implementation("com.google.android.material:material:1.13.0-alpha09")
+    implementation("com.google.android.material:material:1.13.0-alpha12")
 
     // WorkManager
     implementation("androidx.work:work-runtime-ktx:2.10.0")
 
     // Navigation component
-    implementation("androidx.navigation:navigation-fragment-ktx:2.8.5")
-    implementation("androidx.navigation:navigation-ui-ktx:2.8.5")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.9")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.9")
 
     // Lifecycle and ViewModel
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.recyclerview:recyclerview:1.4.0")
 
     // Room
-    val roomVersion = "2.6.1"
+    val roomVersion = "2.7.0"
     implementation("androidx.room:room-runtime:$roomVersion")
     ksp("androidx.room:room-compiler:$roomVersion")
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.7.3" apply false
+    id("com.android.application") version "8.9.1" apply false
     id("org.jetbrains.kotlin.android") version "2.0.0" apply false
     id("com.google.devtools.ksp") version "2.0.0-1.0.22" apply false
-    id("androidx.navigation.safeargs.kotlin") version "2.8.5" apply false
+    id("androidx.navigation.safeargs.kotlin") version "2.8.9" apply false
 }
