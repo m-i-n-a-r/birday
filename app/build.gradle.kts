7d32662cf50c2321a2fc1aae2f129e6411180d40
minar <minar.tastic@gmail.com>
2025-06-14 15:33:56 +0200

    Libraries and Gradle updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,15 +81,15 @@ configurations.configureEach {
 dependencies {
 
     // Default dependencies
-    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.preference:preference-ktx:1.2.1")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("androidx.activity:activity-ktx:1.10.1")
-    implementation("androidx.fragment:fragment-ktx:1.8.6")
+    implementation("androidx.fragment:fragment-ktx:1.8.8")
 
     // Transition
-    implementation("androidx.transition:transition-ktx:1.5.1")
+    implementation("androidx.transition:transition-ktx:1.6.0")
 
     // Constraint / motion layout
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
@@ -101,11 +101,11 @@ dependencies {
     implementation("com.google.android.material:material:1.13.0-alpha12")
 
     // WorkManager
-    implementation("androidx.work:work-runtime-ktx:2.10.0")
+    implementation("androidx.work:work-runtime-ktx:2.10.1")
 
     // Navigation component
-    implementation("androidx.navigation:navigation-fragment-ktx:2.8.9")
-    implementation("androidx.navigation:navigation-ui-ktx:2.8.9")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.9.0")
+    implementation("androidx.navigation:navigation-ui-ktx:2.9.0")
 
     // Lifecycle and ViewModel
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.9.1" apply false
+    id("com.android.application") version "8.10.1" apply false
     id("org.jetbrains.kotlin.android") version "2.0.0" apply false
     id("com.google.devtools.ksp") version "2.0.0-1.0.22" apply false
     id("androidx.navigation.safeargs.kotlin") version "2.8.9" apply false
