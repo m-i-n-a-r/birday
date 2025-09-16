e68ad4f5658e500d52be003af9075269ad1c0eb7
m-i-n-a-r <glucaconti.952@gmail.com>
2025-09-16 13:41:14 +0200

    Fixed build problem

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,6 @@
 plugins {
     id("com.android.application") version "8.13.0" apply false
     id("org.jetbrains.kotlin.android") version "2.2.20" apply false
-    id("com.google.devtools.ksp") version "2.0.0-1.0.22" apply false
+    id("com.google.devtools.ksp") version "2.2.20-2.0.3" apply false
     id("androidx.navigation.safeargs.kotlin") version "2.9.4" apply false
 }
