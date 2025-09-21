7cf02218041caf1aa4184ae40c34fcf382a1a0b4
m-i-n-a-r <glucaconti.952@gmail.com>
2025-09-21 23:48:41 +0200

    Removed unused function

--- app/src/main/java/com/minar/birday/utilities/FileUtils.kt
+++ app/src/main/java/com/minar/birday/utilities/FileUtils.kt
@@ -10,21 +10,6 @@ import com.minar.birday.BuildConfig
 import com.minar.birday.R
 import java.io.File
 
-// Share the backup to a supported app
-fun shareFile(context: Context, fileUri: String) {
-    val file = File(fileUri)
-    val contentUri: Uri =
-        FileProvider.getUriForFile(context, "${context.packageName}.fileprovider", file)
-    val shareIntent = Intent().apply {
-        action = Intent.ACTION_SEND
-        putExtra(Intent.EXTRA_STREAM, contentUri)
-        type = "*/*"
-    }
-    // Verify that the intent will resolve to an activity
-    if (shareIntent.resolveActivity(context.packageManager) != null)
-        context.startActivity(shareIntent)
-}
-
 // Get the URI for a file in the raw folder
 fun getResourceUri(@AnyRes resourceId: Int): Uri =
     Uri.Builder()
