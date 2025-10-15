682f294e4a27cf4d3dca2bb9d97f71ee33126991
m-i-n-a-r <glucaconti.952@gmail.com>
2025-10-15 19:17:58 +0200

    Initial support for ignored events (not clean but whatever)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,5 @@ plugins {
     id("com.android.application") version "8.13.0" apply false
     id("org.jetbrains.kotlin.android") version "2.2.20" apply false
     id("com.google.devtools.ksp") version "2.2.20-2.0.3" apply false
-    id("androidx.navigation.safeargs.kotlin") version "2.9.4" apply false
+    id("androidx.navigation.safeargs.kotlin") version "2.9.5" apply false
 }
